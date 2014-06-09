18589ca22cdcb13b84b7589a63d5256195f54e2a
Brendan Sturges <brendansturges@gmail.com>
2014-06-09 05:20:24 -0700

    adding imgThief project to repo

---
+++ ImgThief/imgThief.py
@@ -0,0 +1,11 @@
+#scope of this project is to create a command line tool that will allow the user to mass download images off of popular image hosting sites (ie, bulk wallpaper posts on imgur.com
+#
+# will prompt user for path to image dump
+# prompt user for directory to save to, or uses a default + generate a folder
+# scan html of page for .png, .jpg or .gif and then make local copies
+# possible plan to create a gui if i feel like it, or maybe embed the script in a webpage
+#
+
+
+
+
